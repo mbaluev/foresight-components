2e8652f3813d90e1c84fd6826b86ce4a83934d5b
mbaluev <mikhail.baluev@gmail.com>
2017-06-08 16:01:03 +0300

    radio

--- common.blocks/radio/radio.js
+++ common.blocks/radio/radio.js
@@ -1,17 +1,4 @@
 $(function(){
-    $('[data-fc="checkbox"]').each(function(){
-        var that = $(this),
-            $input = that.find(".checkbox__input"),
-            $label = that.find(".checkbox__label");
-        if (!$input.prop('disabled')) {
-            $label.on('click', function(e){
-                e.preventDefault();
-                $input.prop('checked') ? $input.prop('checked', false) : $input.prop('checked', true);
-            })
-        }
-    });
-});
-$(function(){
     $('[data-fc="radio"]').each(function(){
         var that = $(this),
             $input = that.find(".radio__input"),
