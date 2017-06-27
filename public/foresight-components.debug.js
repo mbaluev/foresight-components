98d98268f9313a4f715b48614ca140e4318069ed
mbaluev <mikhail.baluev@gmail.com>
2017-06-27 14:04:44 +0300

    update

--- public/foresight-components.debug.js
+++ public/foresight-components.debug.js
@@ -38,13 +38,23 @@ $(function(){
     $('[data-fc="checkbox"]').each(function(){
         var that = $(this),
             $input = that.find(".checkbox__input"),
-            $label = that.find(".checkbox__label");
+            $label = that.find(".checkbox__label"),
+            $button = that.find(".button");
         if (!$input.prop('disabled')) {
-            $label.on('click', function(e){
-                e.preventDefault();
-                $input.prop('checked') ? $input.prop('checked', false) : $input.prop('checked', true);
-                $input.attr('checked') ? $input.removeAttr('checked') : $input.attr('checked', 'checked');
-            })
+            if ($label) {
+                $label.on('click', function(e){
+                    e.preventDefault();
+                    $input.prop('checked') ? $input.prop('checked', false) : $input.prop('checked', true);
+                    $input.attr('checked') ? $input.removeAttr('checked') : $input.attr('checked', 'checked');
+                })
+            }
+            if ($button) {
+                $button.on('click', function(e){
+                    e.preventDefault();
+                    $input.prop('checked') ? $input.prop('checked', false) : $input.prop('checked', true);
+                    $input.attr('checked') ? $input.removeAttr('checked') : $input.attr('checked', 'checked');
+                })
+            }
         }
     });
 });
