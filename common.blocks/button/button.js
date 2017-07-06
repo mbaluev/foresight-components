0eed686dde911ad852d67408f014a6bed456c320
mbaluev <mikhail.baluev@gmail.com>
2017-07-06 19:28:57 +0300

    update

--- common.blocks/button/button.js
+++ common.blocks/button/button.js
@@ -1,5 +1,121 @@
+(function($){
+    var methods = {
+        init : function(options) {
+            return this.each(function(){
+                var self = $(this),
+                    data = self.data('fc-button');
+                if (!data) {
+                    self.data('fc-button', { target : self });
+                    var defaults = {};
+                    var that = this.obj = {};
+                    that.options = $.extend(defaults, options);
+                    that.data = self.data();
+                    that.check = function(){
+                        self.addClass('button_checked');
+                    };
+                    that.uncheck = function(){
+                        self.removeClass('button_checked');
+                    };
+                    that.click = function(){
+                        self.addClass('button_clicked');
+                        self.removeClass('button_clicked_out');
+                    };
+                    that.unclick = function(){
+                        self.addClass('button_clicked_out');
+                        self.removeClass('button_clicked');
+                    };
+                    that.hover = function(){
+                        self.addClass('button_hovered');
+                    };
+                    that.unhover = function(){
+                        self.removeClass('button_hovered');
+                    };
+                    that.enable = function(){
+                        self.removeClass('button_disabled');
+                        self.on('mouseover.button', that.hover);
+                        self.on('mouseout.button', that.unhover);
+                        self.on('mousedown.button touchstart.button', function(){
+                            that.click();
+                            $('body').one('mouseup.button touchend.button', that.unclick);
+                        });
+                        if (self.hasClass('button_toggable_check')) {
+                            self.on('click.button', function(e){
+                                e.preventDefault();
+                                if (self.attr("data-checked") == "true") {
+                                    $(this).removeClass('button_checked');
+                                    self.attr('data-checked', 'false');
+                                } else {
+                                    $(this).addClass('button_checked');
+                                    self.attr('data-checked', 'true');
+                                }
+                            });
+                        }
+                        if (self.hasClass('button_toggable_radio')) {
+                            self.on('click.button', function(e){
+                                e.preventDefault();
+                                if (self.attr("data-checked") != "true") {
+                                    $(this).addClass('button_checked');
+                                    self.attr('data-checked', 'true');
+                                }
+                            });
+                        }
+                        //bind trigger items
+                        if (that.data.toggle == "trigger" && that.data.trigger) {
+                            self.on('click.button', function(e){
+                                e.preventDefault();
+                                self.find('.' + that.data.container).trigger(that.data.trigger);
+                            });
+                        }
+                    };
+                    that.disable = function(){
+                        self.addClass('button_disabled');
+                        self.off('.button');
+                    };
+                    that.start = function(){
+                        if (self.hasClass('button_toggable_check') || self.hasClass('button_toggable_radio')) {
+                            if (that.data.checked) {
+                                that.check();
+                            } else {
+                                that.uncheck();
+                            }
+                        }
+                        if (that.data.disabled) {
+                            that.disable();
+                        } else {
+                            that.enable();
+                        }
+                    };
+                    that.start();
+                }
+                return this;
+            });
+        },
+        enable : function() {
+            return this.each(function() {
+                this.obj.enable();
+            });
+        },
+        disable : function() {
+            return this.each(function() {
+                this.obj.disable();
+            });
+        }
+    };
+    $.fn.button = function( method ) {
+        if ( methods[method] ) {
+            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on $.button' );
+        }
+    };
+})( jQuery );
+
 $(function(){
-    $('[data-fc="button"]').each(function() {
+    $('[data-fc="button"]').button();
+    /*
+    $('[data-fc="button_"]').each(function() {
         var that = $(this),
             data = that.data();
         if (that.hasClass('button_toggable_check') || that.hasClass('button_toggable_radio')) {
@@ -47,7 +163,6 @@ $(function(){
                     }
                 });
             }
-
             //bind trigger items
             if (data.toggle == "trigger" && data.trigger) {
                 that.on('click', function(e){
@@ -57,4 +172,5 @@ $(function(){
             }
         }
     });
+    */
 });
--- common.blocks/tumbler/tumbler.js
+++ common.blocks/tumbler/tumbler.js
@@ -1,5 +1,14 @@
+$.fn.bindFirst = function(name, fn) {
+    this.on(name, fn);
+    this.each(function() {
+        var handlers = $._data(this, 'events')[name.split('.')[0]];
+        var handler = handlers.pop();
+        handlers.splice(0, 0, handler);
+    });
+};
+
 $(function() {
-    $('[data-fc="tumbler"]').each(function () {
+    $('[data-fc="tumbler"]').each(function(){
         var self = $(this),
             $button = self.find('button'),
             $input = self.find('input'),
@@ -13,10 +22,19 @@ $(function() {
             $input.removeAttr('checked');
             $input.prop('checked', false);
         }
-        $button.on('click', function(){
+        self.bindFirst('click.__private__', function(){
             self.toggleClass('tumbler_checked');
             $input.prop('checked') ? $input.prop('checked', false) : $input.prop('checked', true);
             $input.attr('checked') ? $input.removeAttr('checked') : $input.attr('checked', 'checked');
+            data.checked = $input.prop('checked');
         })
+    });
+    $('#tumbler_edit-page').on('click', function(){
+        var self = $(this), data = self.data();
+        if (data.checked) {
+            $('#button_add-widget').button('enable');
+        } else {
+            $('#button_add-widget').button('disable');
+        }
     });
 });
