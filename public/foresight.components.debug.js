ed0533b70eec89ce32e72bb4047a7a3cca4463f8
mbaluev <mikhail.baluev@gmail.com>
2017-08-03 22:08:04 +0300

    update

--- public/foresight.components.debug.js
+++ public/foresight.components.debug.js
@@ -1,6 +1,7 @@
 $(function(){
-    $('.fs-view__middle').bind('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(){
-        $(window).trigger('resize');
+    var timer_resize;
+    $('.fs-view__middle').on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(){
+        clearInterval(timer_resize);
     });
     $('#button_toggle-menu').each(function(){
         var self = $(this),
@@ -11,6 +12,7 @@ $(function(){
             $backdrop = $('<div class="fs-view__backdrop"></div>'),
             onlyloaded = true;
         function show_menu(){
+            timer_resize = setInterval(function(){ $(window).trigger('resize'); }, 100);
             $iconmenu.icon__menu('toggle');
             if (!onlyloaded && !$main.hasClass('fs-view__main_transition')) {
                 $main.addClass('fs-view__main_transition');
@@ -22,6 +24,7 @@ $(function(){
             self.one('click', hide_menu);
         }
         function hide_menu(){
+            timer_resize = setInterval(function(){ $(window).trigger('resize'); }, 100);
             $iconmenu.icon__menu('toggle');
             if (!onlyloaded && !$main.hasClass('fs-view__main_transition')) {
                 $main.addClass('fs-view__main_transition');
