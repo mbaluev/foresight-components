26aa2916cb347516473a95135fc0d649c40e2440
mbaluev <mikhail.baluev@gmail.com>
2017-08-24 22:19:46 +0300

    update

---
+++ public/charts/ktChart/data/orgChartData_template.js
@@ -0,0 +1,23 @@
+/*
+{
+    id: {UserId},
+    parent: {ParentUserId},
+    title: "{FullName}",
+    description: '{Title}',
+    image: "{Photo}",
+    templateName: "contactTemplate",
+    itemTitleColor: "{Color}",
+    termoverdue: {TermOverdue},
+    termout: {TermOut},
+    terminwork: {TermInWork},
+    termdone: {TermDone},
+    budgetoverdue: {BudgetOverdue},
+    budgetinwork: {BudgetInWork},
+    budgetdone: {BudgetDone},
+    risklow: {RiskLow},
+    riskmiddle: {RiskMiddle},
+    riskhigh: {RiskHigh},
+    score: {Score},
+    maxscore: {MaxScore},
+}
+*/
