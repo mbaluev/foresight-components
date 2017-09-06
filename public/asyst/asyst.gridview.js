c085669bf46cc59dae362560ad34b8ca996ca13e
mbaluev <mikhail.baluev@gmail.com>
2017-09-06 16:06:17 +0300

    update

--- public/asyst/asyst.gridview.js
+++ public/asyst/asyst.gridview.js
@@ -4,9 +4,16 @@ Asyst.GridView = function(options){
     that.data = {
         containerid: 'container',
         name: null,
+        
         gridview: null,
         title: '',
-        data: null
+        data: null,
+        header: {
+            views: [],
+            reload: {},
+            settings: [],
+            search: {}
+        }
     };
     that.data = $.extend(that.data, options);
     that.data._el = {
@@ -23,93 +30,23 @@ Asyst.GridView = function(options){
         that.data._el.loader.remove();
     };
 
+    that.init_data = function(){};
     that.load_data = function(callback){
         if (typeof callback == 'function') { callback(); }
     };
+    that.render_data = function(){};
 
     that.init = function(){
         that.loader_add();
+        that.init_data();
         that.load_data(function(){
             that.loader_remove();
             that.data.gridview = new GridView({
                 containerid: that.data.containerid,
                 title: that.data.title,
                 data: that.data.data,
-                header: {
-                    views: [
-                        {
-                            name: 'Мои проекты',
-                            value: 'MyProjects',
-                            selected: true,
-                            onclick: function(){}
-                        },
-                        {
-                            name: 'Все проекты',
-                            value: 'AllProjects',
-                            onclick: function(){}
-                        },
-                        {
-                            name: 'Активные',
-                            value: 'Active',
-                            onclick: function(){}
-                        },
-                        {
-                            name: 'Архивные',
-                            value: 'Archive',
-                            onclick: function(){}
-                        },
-                        {
-                            name: 'Бюджеты проектов',
-                            value: 'Budgets',
-                            onclick: function(){}
-                        },
-                        {
-                            name: 'Проекты без целей',
-                            value: 'NoGoals',
-                            onclick: function(){}
-                        }
-                    ],
-                    reload: {
-                        onclick: function(){ console.log('reload'); }
-                    },
-                    settings: [
-                        {
-                            icon: 'icon_svg_plus',
-                            name: 'Добавить',
-                            onclick: function(){ console.log('add'); }
-                        },
-                        {
-                            icon: 'icon_svg_trash',
-                            name: 'Удалить',
-                            onclick: function(){}
-                        },
-                        {
-                            icon: 'icon_svg_expand',
-                            name: 'Развернуть все группы',
-                            onclick: function(){}
-                        },
-                        {
-                            icon: 'icon_svg_collapse',
-                            name: 'Свернуть все группы',
-                            onclick: function(){}
-                        },
-                        {
-                            icon: 'icon_svg_export',
-                            name: 'Экспорт',
-                            onclick: function(){}
-                        },
-                        {
-                            icon: 'icon_svg_search',
-                            name: 'Расширенный фильтр',
-                            onclick: function(){}
-                        }
-                    ],
-                    search: {
-                        onkeyup: function(){ console.log('searching'); },
-                        onclear: function(){ console.log('clear'); }
-                    }
-                },
-                render: render_grid
+                header: that.data.header,
+                render: that.render_data
             });
         });
     };
