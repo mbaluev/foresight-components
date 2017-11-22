6c54feed838609dd1bcf42d3bd5da98a03ce2758
mbaluev <mikhail.baluev@gmail.com>
2017-11-22 14:26:33 +0300

    update

--- common.blocks/calendar/calendar.js
+++ common.blocks/calendar/calendar.js
@@ -97,7 +97,7 @@
                                         that.data._el.calendar__row_top.append(
                                             that.data._el.calendar__datepicker
                                         ),
-                                        (that.data.columns ? that.data._el.calendar__row_bottom.append(
+                                        (that.data.columns.length > 0 ? that.data._el.calendar__row_bottom.append(
                                             that.data._el.calendar__table
                                         ) : null)
                                     )
