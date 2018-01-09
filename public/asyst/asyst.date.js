c414096d7f13217f00404c622f46f209b93bceac
mbaluev <mikhail.baluev@gmail.com>
2018-01-09 14:11:32 +0300

    update

--- public/asyst/asyst.date.js
+++ public/asyst/asyst.date.js
@@ -2,7 +2,7 @@ if (!Asyst.date) { Asyst.date = {}; }
 Asyst.date.convertToGenitive = function(dateStr){
     var lastChar = dateStr.slice(-1);
     if (lastChar == 'ь' || lastChar == 'я') {
-        return dateStr.substring(dateStr.length - 1) + 'я';
+        return dateStr.substr(0, dateStr.length - 1) + 'я';
     } else {
         return dateStr + 'а';
     }
