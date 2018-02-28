cfe40347d834d907e9c6114dbd3c816b1560d5af
mbaluev <mikhail.baluev@gmail.com>
2018-02-28 14:08:07 +0300

    update

--- public/pages/gridview.js
+++ public/pages/gridview.js
@@ -534,7 +534,7 @@ var GridView3 = function(options){
         that.data._el.menu__list.find('.menu__item-link').on('click', function(){
             if (!that.data.loading) {
                 that.data._el.menu__list.find('.menu__item-link').addClass('menu__item-link_disabled');
-                $(this).removeClass('menu__item-link_selected');
+                $(this).removeClass('menu__item-link_disabled');
                 that.data._el.menu__list.find('.menu__item-link').removeClass('menu__item-link_selected');
                 $(this).addClass('menu__item-link_selected');
                 var value = $(this).data('value');
