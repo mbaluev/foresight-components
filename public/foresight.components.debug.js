4f7233565acb074fe8335f8282dd8138d7cc73a8
mbaluev <mikhail.baluev@gmail.com>
2018-03-21 16:45:37 +0300

    update

--- public/foresight.components.debug.js
+++ public/foresight.components.debug.js
@@ -2481,7 +2481,7 @@ $(function(){
                     that.counttime = function(){
                         render();
                         setInterval(function(){
-                            //render();
+                            render();
                         }, 1e3);
                         function pad(a,b){ return([1e15]+a).slice(-b); }
                         function render(){
