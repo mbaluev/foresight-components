e1b43cfd3efd9eb71fcae9b8c07be6e9d014b217
mbaluev <mikhail.baluev@gmail.com>
2018-04-13 15:30:09 +0300

    update

--- public/asyst/asyst.dashboard.js
+++ public/asyst/asyst.dashboard.js
@@ -38,7 +38,7 @@ Asyst.PageDashboard = function(options){
             },
             dbWidget: {
                 library: [],
-                loader: null
+                loader: Asyst.MetaElementLoader
             },
             dbChartType: {
                 library: [],
