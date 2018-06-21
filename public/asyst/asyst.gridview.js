75ce98ce5507b8761d4ccdc8fa732178f2002835
mbaluev <mikhail.baluev@gmail.com>
2018-06-21 17:33:05 +0300

    update

--- public/asyst/asyst.gridview.js
+++ public/asyst/asyst.gridview.js
@@ -577,7 +577,7 @@ Asyst.GridView = function(options){
                         Asyst.Workspace.views[that.data.viewname].isViewSampled) {
                         that.data.grid.saveCurrent();
                         that.data.params.viewSampleId = undefined;
-                        that.data._el.select__view_sample.select('uncheck_all');
+                        that.data._el.select__view_sample.select('destroy');
                     }
                     setPageCookie('CurrentViewName' + (that.data.params.entity ? '_' + that.data.params.entity : ''), key);
                     that.data.viewname = key;
