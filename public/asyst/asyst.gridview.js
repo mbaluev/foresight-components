8c20b771885f783c2b2caf88dd79b983c845ea5a
mbaluev <mikhail.baluev@gmail.com>
2018-06-25 15:33:38 +0300

    update

--- public/asyst/asyst.gridview.js
+++ public/asyst/asyst.gridview.js
@@ -1044,7 +1044,7 @@ Asyst.GridView = function(options){
                         // восстанавливаем значения фильтров
                         if (that.data.filter.filterArgs) {
                             if (that.data.filter.filterArgs.oper) {
-                                _control.caption.find('.radio[value="' + that.data.filter.filterArgs.oper + '"]').radio('check');
+                                _control.caption.find('.radioFilterType').radio_group('check', that.data.filter.filterArgs.oper);
                             }
                             if (that.data.filter.filterArgs.filterItems) {
                                 that.data.filter.filterArgs.filterItems.map(function(d){
