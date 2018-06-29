1a56db15eaa45c82094781e93a6406dca94ff968
mbaluev <mikhail.baluev@gmail.com>
2018-06-29 13:59:56 +0300

    update

--- public/foresight.pages.debug.js
+++ public/foresight.pages.debug.js
@@ -2099,12 +2099,16 @@ var GridView2 = function(options){
     that.menu__item_lock = function(){
         that.data._el.radio_group.radio_group('disable');
         that.data._el.input_search.input('disable');
-        that.data._el.buttons.button('disable');
+        that.data._el.buttons.map(function(){
+            $(this).button('disable');
+        });
     };
     that.menu__item_unlock = function(){
         that.data._el.radio_group.radio_group('enable');
         that.data._el.input_search.input('enable');
-        that.data._el.buttons.button('enable');
+        that.data._el.buttons.map(function(){
+            $(this).button('enable');
+        });
     };
 
     that.loader_add = function(){
