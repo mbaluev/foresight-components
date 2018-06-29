bc653068d6a507859b00ad08e447d92a0aa5e257
mbaluev <mikhail.baluev@gmail.com>
2018-06-29 13:41:41 +0300

    update

--- public/pages/gridview.js
+++ public/pages/gridview.js
@@ -194,14 +194,18 @@ var GridView = function(options){
         that.data._el.button_reload.button('disable');
         that.data._el.button_settings.button('disable');
         that.data._el.input_search.input('disable');
-        that.data._el.button_extFilter.button('disable');
+        if (that.data._el.button_extFilter) {
+            that.data._el.button_extFilter.button('disable');
+        }
     };
     that.menu__item_unlock = function(){
         that.data._el.select_view.select('enable');
         that.data._el.button_reload.button('enable');
         that.data._el.button_settings.button('enable');
         that.data._el.input_search.input('enable');
-        that.data._el.button_extFilter.button('enable');
+        if (that.data._el.button_extFilter) {
+            that.data._el.button_extFilter.button('enable');
+        }
     };
 
     that.loader_add = function(){
