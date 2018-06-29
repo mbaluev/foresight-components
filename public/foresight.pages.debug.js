935528b97b0f42fbc7b88aef1aa1d063d170d297
mbaluev <mikhail.baluev@gmail.com>
2018-06-29 13:18:54 +0300

    update

--- public/foresight.pages.debug.js
+++ public/foresight.pages.debug.js
@@ -2084,6 +2084,13 @@ var GridView2 = function(options){
         }
     };
 
+    that.menu__item_lock = function(){
+        that.data._el.radio_group.radio_group('disable');
+    };
+    that.menu__item_unlock = function(){
+        that.data._el.radio_group.radio_group('enable');
+    };
+
     that.loader_add = function(){
         that.data._el.target.before(that.data._el.loader)
     };
